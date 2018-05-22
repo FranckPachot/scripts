418b2749de870d12fec181a392fbb5d4e6e74649
Franck Pachot <git@pachot.net>
2018-05-22 22:42:46 +0200

    comments

--- statspack/01-install.sql
+++ statspack/01-install.sql
@@ -1,8 +1,10 @@
 set echo on
 whenever sqlerror exit failure
+-- replace +DATA by the ASM diskgroup ot the datafile for STATSPACK datafile
 create tablespace STATSPACK datafile '+DATA' size 100M autoextend on maxsize 2G;
 define default_tablespace='STATSPACK'
 define temporary_tablespace='TEMP'
+-- we set a random password. No need to know it as we will connect through sys
 column random new_value perfstat_password noprint
 select '"'||dbms_random.string('a',30)||'"' random from dual;
 alter session set "_oracle_script"=true;
