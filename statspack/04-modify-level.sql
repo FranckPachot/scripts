d0c694d660e5601f25565714d666a8f307b45eed
@FranckPachot <33070466+FranckPachot@users.noreply.github.com>
2018-06-27 11:29:24 +0000

    Update 04-modify-level.sql

--- statspack/04-modify-level.sql
+++ statspack/04-modify-level.sql
@@ -1,3 +1,5 @@
 set timing on
 exec statspack.snap(i_snap_level=>7);
 exec STATSPACK.MODIFY_STATSPACK_PARAMETER (i_snap_level=>7, i_instance_number=>null);
+exec statspack.snap(i_snap_level=>7);
+exec dbms_stats.gather_schema_stats('PERFSTAT');
