d14c2ed86e0b876530d4890fd759c9f74646b159
@FranckPachot <33070466+FranckPachot@users.noreply.github.com>
2018-08-07 20:56:17 +0200

    Update segment_free_space_sql.sql

--- administration/segment_free_space_sql.sql
+++ administration/segment_free_space_sql.sql
@@ -1,7 +1,7 @@
 with function freebytes(segment_owner varchar2, segment_name varchar2, segment_type varchar2,partition_name varchar2) return number as
  unf number; unfb number; fs1 number; fs1b number; fs2 number; fs2b number; fs3 number; fs3b number; fs4 number; fs4b number; full number; fullb number; 
 begin
- dbms_space.space_usage(segment_owner,segment_name,segment_type,unf,unfb,fs1,fs1b,fs2,fs2b,fs3,fs3b,fs4,fs4b,full,fullb,partition_name=&gt;partition_name);
+ dbms_space.space_usage(segment_owner,segment_name,segment_type,unf,unfb,fs1,fs1b,fs2,fs2b,fs3,fs3b,fs4,fs4b,full,fullb,partition_name=>partition_name);
  return unfb+fs1b+fs2b*0.25+fs3b*0.5+fs4b*0.75;
 end;
 select round(freebytes(owner,segment_name,segment_type,partition_name)/1024/1024/1024,3) free_GB,segment_type,owner,segment_name,partition_name
