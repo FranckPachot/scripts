2303f750cf1d7596f7557a4cf6f902269ebdede8
@FranckPachot <33070466+FranckPachot@users.noreply.github.com>
2018-08-07 20:51:19 +0200

    Update segment_free_space_plsql.sql

--- administration/segment_free_space_plsql.sql
+++ administration/segment_free_space_plsql.sql
@@ -7,7 +7,7 @@ begin
  ) order by bytes desc) where 10>=rownum)
  loop
   begin
-   dbms_space.space_usage(i.owner,i.segment_name,i.segment_type,unf,unfb,fs1,fs1b,fs2,fs2b,fs3,fs3b,fs4,fs4b,full,fullb,partition_name=&gt;i.partition_name);
+   dbms_space.space_usage(i.owner,i.segment_name,i.segment_type,unf,unfb,fs1,fs1b,fs2,fs2b,fs3,fs3b,fs4,fs4b,full,fullb,partition_name=>i.partition_name);
    dbms_output.put_line(to_char((unfb+fs1b+fs2b*0.25+fs3b*0.5+fs4b*0.75)/1024/1024/1024,'999G999D999')||' GB free in '||i.segment_type||' "'||i.owner||'"."'||i.segment_name||'" partition "'||i.partition_name||'"');
   exception
    when others then dbms_output.put_line(i.segment_type||' "'||i.owner||'"."'||i.segment_name||'" partition "'||i.partition_name||'": '||sqlerrm);
