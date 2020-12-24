03f52166d9ac24d2c187c478c3d0b9c15d5c1abc
@FranckPachot <33070466+FranckPachot@users.noreply.github.com>
2020-12-24 10:23:42 +0100

    Update 02-schedule.sql
    Added a light job every 5 minutes in snal level 0

--- statspack/02-schedule.sql
+++ statspack/02-schedule.sql
@@ -3,11 +3,13 @@
 */
 DECLARE
     instno NUMBER;
-    snapjob VARCHAR2(30);
-    purgejob VARCHAR2(30);
+    snapjob VARCHAR2(128);
+    lightjob VARCHAR2(128);
+    purgejob VARCHAR2(128);
 BEGIN
     select instance_number into instno from v$instance;
     snapjob  := 'PERFSTAT.STATSPACK_SNAP_' || instno;
+    lightjob  := 'PERFSTAT.STATSPACK_SNAP_LIGHT_' || instno;
     purgejob := 'PERFSTAT.STATSPACK_PURGE_' || instno;
  
     DBMS_SCHEDULER.CREATE_JOB (
@@ -21,7 +23,19 @@ BEGIN
        enabled => TRUE,
        auto_drop => FALSE,
        comments => 'Take hourly Statspack snapshot');
- 
+       
+    DBMS_SCHEDULER.CREATE_JOB (
+       job_name => lightjob,
+       job_type => 'PLSQL_BLOCK',
+       job_action => 'statspack.snap(i_snap_level=>0);',
+       number_of_arguments => 0,
+       start_date => systimestamp,
+       repeat_interval => 'FREQ=HOURLY; BYMINUTE=5,10,15,20,25,30,35,40,45,50,55',
+       end_date => NULL,
+       enabled => TRUE,
+       auto_drop => FALSE,
+       comments => 'Take frequent Statspack snapshot level 0');
+       
     DBMS_SCHEDULER.CREATE_JOB (
        job_name => purgejob,
        job_type => 'PLSQL_BLOCK',
@@ -37,6 +51,10 @@ BEGIN
     DBMS_SCHEDULER.SET_ATTRIBUTE( name => snapjob, attribute => 'logging_level', value => DBMS_SCHEDULER.LOGGING_OFF );
     DBMS_SCHEDULER.SET_ATTRIBUTE( name => snapjob, attribute => 'INSTANCE_ID', value=>instno);
     DBMS_SCHEDULER.SET_ATTRIBUTE( name => snapjob, attribute => 'INSTANCE_STICKINESS', value=>TRUE);
+    
+    DBMS_SCHEDULER.SET_ATTRIBUTE( name => lightjob, attribute => 'logging_level', value => DBMS_SCHEDULER.LOGGING_OFF );
+    DBMS_SCHEDULER.SET_ATTRIBUTE( name => lightjob, attribute => 'INSTANCE_ID', value=>instno);
+    DBMS_SCHEDULER.SET_ATTRIBUTE( name => lightjob, attribute => 'INSTANCE_STICKINESS', value=>TRUE);
  
     DBMS_SCHEDULER.SET_ATTRIBUTE( name => purgejob, attribute => 'logging_level', value => DBMS_SCHEDULER.LOGGING_OFF );
     DBMS_SCHEDULER.SET_ATTRIBUTE( name => purgejob, attribute => 'INSTANCE_ID', value=>instno);
